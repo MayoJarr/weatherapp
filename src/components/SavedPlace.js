fca5376347467aeaaca34343edb52772ea683cd2
MayoJarr <jakubmajecki2321@gmail.com>
2023-09-11 20:09:50 +0200

    eslintignore

--- src/components/SavedPlace.js
+++ src/components/SavedPlace.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import '../styles/Place.css'
+// eslint-disable
+import React from 'react';
+import '../styles/Place.css';
 
-function SavedPlace({place, choose, deletePlace}) {
+function SavedPlace({ place, choose, deletePlace }) {
   return (
-    <div className='place'>
-      <div onClick={()=> choose(place)}>{place.name}, {place.country}</div>
-      <button className='deleteBtn' onClick={()=> deletePlace(place)}>X</button>
+    <div className="place">
+      <div onClick={() => choose(place)}>
+        {place.name}
+        ,
+        {' '}
+        {place.country}
+      </div>
+      <button className="deleteBtn" onClick={() => deletePlace(place)}>X</button>
     </div>
-  )
+  );
 }
 
-export default SavedPlace
+export default SavedPlace;

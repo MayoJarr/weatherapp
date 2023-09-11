4f470741728e2983dd886220e689af91e2783441
MayoJarr <jakubmajecki2321@gmail.com>
2023-09-11 19:01:06 +0200

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,112 +1,73 @@
-import "./styles/App.css";
-import Day from "./components/Day";
-import { useEffect, useState } from "react";
-import Place from "./components/Place";
-import Hour from "./components/Hour";
+/* eslint-disable no-plusplus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/button-has-type */
+/* eslint-disable prefer-const */
+import React, { useEffect, useState } from 'react';
+import './styles/App.css';
+import Day from './components/Day';
+import Place from './components/Place';
+import Hour from './components/Hour';
 
 function App() {
-  const apikey = "316cd4780f98abf47235cb4db1137e54";
+  const apikey = '316cd4780f98abf47235cb4db1137e54';
   let [places, setPlaces] = useState([]);
   let [currentPlace, setCurrentPlace] = useState(null);
   let [searchResults, setSeatchResults] = useState([]);
 
-  let lon = 0;
-  let lat = 0;
-
   let [isPlacesVisible, setIsPlacesVisible] = useState(false);
 
-  useEffect(() => {
-    test();
-  }, []);
   //  CURRENT WEATHER
   const fetchDataFromApi = async (la, lo) => {
-    let fetchData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=${apikey}&units=metric`
+    const fetchData = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=${apikey}&units=metric`,
     );
-    let data = await fetchData.json();
+    const data = await fetchData.json();
     return data;
   };
   // FORECAST
   const getForecast = async (la, lo) => {
-    let fetchData = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${la}&lon=${lo}&appid=${apikey}&units=metric`
+    const fetchData = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${la}&lon=${lo}&appid=${apikey}&units=metric`,
     );
-    let data = await fetchData.json();
+    const data = await fetchData.json();
     return data;
   };
   // SEARCH LOCALISATION
-  const search = async localisation => {
+  const search = async (localisation) => {
     if (localisation.length <= 0) {
       return;
-    } else if (localisation.length > 0) {
-      let fetchData = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${localisation}&limit=5&appid=${apikey}`
-      );
-      let data = await fetchData.json();
-      setSeatchResults((searchResults = data));
     }
-  };
-  // SELECT LOCALISATION
-  const addPlace = async e => {
-    lon = e.lon.toFixed(2);
-    lat = e.lat.toFixed(2);
-    let data = await fetchDataFromApi(e.lat, e.lon);
-    let foreData = await getForecast(e.lat, e.lon);
-    setPlaces(
-      places.concat({
-        name: e.name,
-        lat: e.lat,
-        lon: e.lon,
-        country: e.country,
-        temp: data.main.temp,
-        forecast: formatData(foreData),
-        hourly: formatDataToHours(foreData),
-      })
+    // } else if (localisation.length > 0) {
+    const fetchData = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${localisation}&limit=5&appid=${apikey}`,
     );
+    const data = await fetchData.json();
+    setSeatchResults((searchResults = data));
+    // }
   };
-  const choosePlace = e => {
-    lon = e.lon.toFixed(2);
-    lat = e.lat.toFixed(2);
-    let eo = places.findIndex(place => place.lon === e.lon);
+  const choosePlace = (e) => {
+    const eo = places.findIndex((place) => place.lon === e.lon);
     setCurrentPlace((currentPlace = places[eo]));
   };
-  const test = async () => {
-    let data = await fetchDataFromApi(51.10, 17.03);
-    let foreData = await getForecast(51.10, 17.03);
-    setPlaces(
-      (places = [
-        {
-          name: "Wroclaw",
-          lat: 51.10,
-          lon: 17.03,
-          country: "PL",
-          temp: data.main.temp,
-          forecast: formatData(foreData),
-          hourly: formatDataToHours(foreData),
-        },
-      ])
-    );
-    setCurrentPlace((currentPlace = places[0]));
-    formatDataToHours(foreData);
-  };
-  const getAvgTempOfDay = days => {
-    let sum = 0;
-    days.forEach(hour => {
-      sum += hour.main.temp;
-    });
-    return sum / days.length;
-  };
-  const getHighestOfDay = day => {
-    return day.reduce((acc, curr) => (curr.main.temp > acc ? curr.main.temp : acc), 0);
-  };
 
-  const formatData = data => {
-    data.list.map(e => (e.dt = new Date(e.dt * 1000)));
+  // const getAvgTempOfDay = (days) => {
+  //   let sum = 0;
+  //   days.forEach((hour) => {
+  //     sum += hour.main.temp;
+  //   });
+  //   return sum / days.length;
+  // };
+  // eslint-disable-next-line max-len
+  const getHighestOfDay = (day) => day.reduce((acc, curr) => (curr.main.temp > acc ? curr.main.temp : acc), 0);
+  const formatData = (data) => {
+    // eslint-disable-next-line no-return-assign
+    data.list.map((e) => (e.dt = new Date(e.dt * 1000)));
     let prev = data.list[0].dt.getDate();
     let curr;
-    let eo = [[]];
+    const eo = [[]];
     let num = 0;
-    data.list.forEach(e => {
+    data.list.forEach((e) => {
       curr = e.dt.getDate();
       if (curr === prev) {
         eo[num].push(e);
@@ -117,47 +78,90 @@ function App() {
       }
       prev = e.dt.getDate();
     });
-    let thing = [];
-    eo.forEach(day => {
-      let dayMiddle = Number((day.length / 2).toFixed(0))
+    const thing = [];
+    eo.forEach((day) => {
+      const dayMiddle = Number((day.length / 2).toFixed(0));
       thing.push({
-        name: day[0].dt.toLocaleString("en-en", { weekday: "long" }),
+        name: day[0].dt.toLocaleString('en-en', { weekday: 'long' }),
         // temp: getAvgTempOfDay(day).toFixed(1),
         temp: getHighestOfDay(day).toFixed(0),
         date: day[0].dt.getDate(),
-        icon: day[dayMiddle === 1 ? 0 : dayMiddle].weather[0].icon
+        icon: day[dayMiddle === 1 ? 0 : dayMiddle].weather[0].icon,
       });
     });
     return thing;
   };
-  const formatDataToHours = data => {
-    let arr = [];
+  const formatDataToHours = (data) => {
+    const arr = [];
     let hour = 0;
-    data.list.forEach(item => {
-      hour = item.dt.getHours().toString() + ":00";
+    data.list.forEach((item) => {
+      hour = `${item.dt.getHours().toString()}:00`;
       if (hour.length === 4) {
-        hour = "0" + hour;
+        hour = `0${hour}`;
       }
-      arr.push({ hour: hour, temp: item.main.temp.toFixed(0), icon: item.weather[0].icon });
+      arr.push({ hour, temp: item.main.temp.toFixed(0), icon: item.weather[0].icon });
     });
     return arr;
   };
-  const deletePlace = place => setPlaces(places.filter(element => element.lat != place.lat));
+  const test = async () => {
+    const data = await fetchDataFromApi(51.10, 17.03);
+    const foreData = await getForecast(51.10, 17.03);
+    setPlaces(
+      (places = [
+        {
+          name: 'Wroclaw',
+          lat: 51.10,
+          lon: 17.03,
+          country: 'PL',
+          temp: data.main.temp,
+          forecast: formatData(foreData),
+          hourly: formatDataToHours(foreData),
+        },
+      ]),
+    );
+    // eslint-disable-next-line prefer-destructuring
+    setCurrentPlace((currentPlace = places[0]));
+    formatDataToHours(foreData);
+  };
+  // SELECT LOCALISATION
+  const addPlace = async (e) => {
+    const data = await fetchDataFromApi(e.lat, e.lon);
+    const foreData = await getForecast(e.lat, e.lon);
+    setPlaces(
+      places.concat({
+        name: e.name,
+        lat: e.lat,
+        lon: e.lon,
+        country: e.country,
+        temp: data.main.temp,
+        forecast: formatData(foreData),
+        hourly: formatDataToHours(foreData),
+      }),
+    );
+  };
 
-  const togglePlaces = () => setIsPlacesVisible((isPlacesVisible = !isPlacesVisible));
+  const deletePlace = (place) => setPlaces(places.filter((element) => element.lat !== place.lat));
+
+  const togglePlaces = () => {
+    setIsPlacesVisible((isPlacesVisible = !isPlacesVisible));
+  };
 
   let i = 1;
+
+  useEffect(() => {
+    test();
+  }, []);
 
   return (
     <div className="App">
       <div
-        className={isPlacesVisible ? "overlay overlayActive" : "overlay"}
+        className={isPlacesVisible ? 'overlay overlayActive' : 'overlay'}
         onClick={() => togglePlaces()}
-      ></div>
+      />
       <button className="placesBtn" onClick={() => togglePlaces()}>
         |||
       </button>
-      <div className={isPlacesVisible ? "Places active" : "Places"}>
+      <div className={isPlacesVisible ? 'Places active' : 'Places'}>
         <Place
           places={places}
           searchResult={searchResults}
@@ -170,33 +174,35 @@ function App() {
       <div className="Degrees">
         <div className="degreeContainer">
           <div className="placeName">
-            {currentPlace != null ? `${currentPlace.name}, ${currentPlace.country}` : "no data"}
+            {currentPlace != null ? `${currentPlace.name}, ${currentPlace.country}` : 'no data'}
           </div>
           <div className="placeDegrees">
-            {currentPlace != null ? currentPlace.temp.toFixed(0): "no data"}&#8451;
+            {currentPlace != null ? currentPlace.temp.toFixed(0) : 'no data'}
+            &#8451;
           </div>
         </div>
       </div>
       <div className="Hours">
         {currentPlace != null
-          ? currentPlace.hourly.map(item => <Hour hour={item.hour} temp={item.temp} icon={item.icon} key={i++} />)
-          : "no data"}
+          // eslint-disable-next-line max-len
+          ? currentPlace.hourly.map((item) => <Hour hour={item.hour} temp={item.temp} icon={item.icon} key={i++} />)
+          : 'no data'}
       </div>
       <div className="Days">
         {currentPlace != null
-          ? currentPlace.forecast.map(item => (
-              <Day dayName={item.name} degrees={item.temp} icon={item.icon} key={item.date} />
-            ))
-          : "no data"}
+          ? currentPlace.forecast.map((item) => (
+            <Day dayName={item.name} degrees={item.temp} icon={item.icon} key={item.date} />
+          ))
+          : 'no data'}
       </div>
       <div className="footer">
-        Copyright © 2023 Jakub Majecki <a href="https://github.com/MayoJarr">MayoJarr</a>
+        Copyright © 2023 Jakub Majecki
+        {' '}
+        <a href="https://github.com/MayoJarr">MayoJarr</a>
       </div>
     </div>
   );
 }
-
 export default App;
-
-//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key} geocoding
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=316cd4780f98abf47235cb4db1137e54&units=metric 5 days 3 hours
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key} geocoding
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=316cd4780f98abf47235cb4db1137e54&units=metric 5 days 3 hours
